b56dc164d42277c5cce05ac751d39eb44dc31be5
Will Scott <will.scott@protocol.ai>
2022-10-24 12:30:10 +0200

    update to fixed prod

--- _next/static/E-YKV1aMt4OrNlGxszJ5B/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/367-54cd81672ad9776e.js","static/chunks/pages/index-1047f68f7f696c18.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
